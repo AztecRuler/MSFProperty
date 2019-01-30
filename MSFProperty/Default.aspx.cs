529608cb7f9e75dd6da35abf2936b0f26ae7b5c2
Gerry Sweeny <gfs_god@yahoo.com>
2019-01-30 17:59:57 +0000

    started on repeater pagination

--- MSFProperty/Default.aspx.cs
+++ MSFProperty/Default.aspx.cs
@@ -12,6 +12,7 @@ namespace MSFProperty
     public partial class Default : System.Web.UI.Page
     {
         List<string> editableTextContents = new List<string>();
+        private List<int> pagerNumbersList = new List<int>(); 
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -19,18 +20,54 @@ namespace MSFProperty
             SetNumberOfProperty();
         }
 
-        protected void SetNumberOfProperty()
+        protected void SetPagination()
         {
+            
             string selectedNumber = PagerPropHome.SelectedValue;
             int.TryParse(selectedNumber, out int number);
 
+            //int numBoxes = Math.DivRem(numItems, capacity, out remainder);
+            var rowCount = 0;
+            var rowCountItteration = 1; 
+            //get all properties count 
             using (var db = new Model1())
             {
-                PropertyRepeaterHome.DataSource = db.Properties.Where(p => p.Featured == true).ToList().Take(number);
-
-                PropertyRepeaterHome.DataBind();
-
+                rowCount = db.Properties.Count(p => p.Featured == true);
+          
             }
+            //loop through them and find how many sets using the number to display 
+            for (int i = 0; i < rowCount; i++)
+            {
+                //for each set add 1 to the pager numbers;
+                
+                if (i % number == 0)
+                {
+                    pagerNumbersList.Add(rowCountItteration);
+                    rowCountItteration++;
+                }
+            }
+            //add this list as data for reapeater 
+
+            pagerRepeater.DataSource = pagerNumbersList;
+
+            pagerRepeater.DataBind();
+        }
+        protected void SetNumberOfProperty(string indexIn = "0")
+        {
+            string selectedNumber = PagerPropHome.SelectedValue;
+            int.TryParse(selectedNumber, out int number);
+            string indexNumber = indexIn;
+            int.TryParse(indexNumber, out int index);
+
+            index = index * number; 
+            using (var db = new Model1())
+            {
+                PropertyRepeaterHome.DataSource = db.Properties.Where(p => p.Featured == true).ToList().Skip(index - number).Take(number);
+                 PropertyRepeaterHome.DataBind();
+                    
+            }
+            SetPagination();
+
         }
 
         public string getContents(int id)
--- MSFProperty/Default.aspx.designer.cs
+++ MSFProperty/Default.aspx.designer.cs
@@ -47,5 +47,23 @@ namespace MSFProperty {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater PropertyRepeaterHome;
+        
+        /// <summary>
+        /// pagerRepeater control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater pagerRepeater;
+        
+        /// <summary>
+        /// pagerNumber control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField pagerNumber;
     }
 }
